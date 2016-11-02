3bad3a22d0bba6a0750d99e1172bb90476bed1a1
takanorip <takanori0170@gmail.com>
2016-11-02 14:42:22 +0900

    initial

---
+++ scripts/App.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import { render } from 'react-dom';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import injectTapEventPlugin from 'react-tap-event-plugin';
+import AppBar from 'material-ui/AppBar';
+import Drawer from 'material-ui/Drawer';
+import MenuItem from 'material-ui/MenuItem';
+
+import styles from './App.css';
+
+injectTapEventPlugin();
+
+class App extends Component {
+
+  render() {
+    return ();
+  }
+}
+
+render(<App />, document.getElementById('app'));
---
+++ webpack.config.js
@@ -0,0 +1,42 @@
+module.exports = {
+  entry: {
+    js: './scripts/App.js',
+    html: './scripts/index.html',
+  },
+  output: {
+    path: `${__dirname}/out`,
+    filename: 'bundle.js',
+  },
+  devServer: {
+    contentBase: 'out',
+    port: 3000,
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/,
+        query: {
+          presets: ['es2015', 'react'],
+          plugins: [],
+        },
+      },
+      {
+        test: /\.css$/,
+        loader: 'style-loader!css-loader',
+      },
+      {
+        test: /\.html$/,
+        loader: 'file?name=[name].[ext]',
+      },
+      {
+        test: /\.jpg$/,
+        loader: 'file?name=[path][name].[ext]'
+      },
+    ],
+  },
+  resolve: {
+    modulesDirectories: ['node_modules', './scripts'],
+  },
+};
